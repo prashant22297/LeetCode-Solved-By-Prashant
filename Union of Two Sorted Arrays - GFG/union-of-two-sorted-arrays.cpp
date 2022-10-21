421d521827db9b59d27338e02107767de59bada0
Prashant Kumar Singh <prashant22297@gmail.com>
2022-10-21 09:31:52 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -12,35 +12,45 @@ class Solution{
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
         //used set here and later we converted the set to vector , just to avoid duplicates . 
-        set<int> ans;
-        int i=0,j=0;
-        while(i<n && j<m){
-            if(arr1[i]>arr2[j]){
-                ans.insert(arr2[j]);
-                j++;
-            }
-            else if(arr1[i]<arr2[j]){
-                ans.insert(arr1[i]);
-                i++;
-            }
-            else {
-                ans.insert(arr1[i]);
-                i++;
-                j++;
-            }
-        }
-        while(i<n){
-            ans.insert(arr1[i]);
-            i++;
-        }
-        while(j<m){
-            ans.insert(arr2[j]);
-            j++;
-        }
-        vector<int> temp;
-        for(auto t:ans)
-        temp.push_back(t);
-        return temp;
+        // set<int> ans;
+        // int i=0,j=0;
+        // while(i<n && j<m){
+        //     if(arr1[i]>arr2[j]){
+        //         ans.insert(arr2[j]);
+        //         j++;
+        //     }
+        //     else if(arr1[i]<arr2[j]){
+        //         ans.insert(arr1[i]);
+        //         i++;
+        //     }
+        //     else {
+        //         ans.insert(arr1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // while(i<n){
+        //     ans.insert(arr1[i]);
+        //     i++;
+        // }
+        // while(j<m){
+        //     ans.insert(arr2[j]);
+        //     j++;
+        // }
+        // vector<int> temp;
+        // for(auto t:ans)
+        // temp.push_back(t);
+        // return temp;
+        
+        set<int> temp;
+        for(int i=0;i<n;i++)
+            temp.insert(arr1[i]);
+        for(int i=0;i<m;i++)
+            temp.insert(arr2[i]);
+        vector<int> ans;
+        for(auto t:temp)
+        ans.push_back(t);
+        return ans;
     }
 };
 
