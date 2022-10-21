9364ac4fa8e5bc01f88e2e45a7ab9f23d241113f
Prashant Kumar Singh <prashant22297@gmail.com>
2022-10-21 18:37:04 +0530

    Time: 3 ms (68.23%), Space: 8.8 MB (61.42%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void add_one(vector<int> & nums,int n){
+        if(nums.size()==0){
+            nums.push_back(1);
+            return;
+        }
+        int t = nums[n-1];
+        nums.pop_back();
+        add_one(nums,n-1);
+        nums.push_back(t);
+    }
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=0;
+        int n=digits.size();
+        int t= digits[n-1]+1;
+        carry=t/10;
+        digits[n-1]=t%10;
+        if(carry==1){
+            for(int i=n-2;i>=0;i--){
+                t=digits[i]+carry;
+                carry=t/10;
+                digits[i]=t%10;
+            }
+        }
+        if(carry) add_one(digits,n);
+        return digits;
+    }
+};
