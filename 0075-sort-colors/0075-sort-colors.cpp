c4f468cd7c400e7f9a3b5f7385e0fb3e2b9746ee
Prashant Kumar Singh <prashant22297@gmail.com>
2022-10-23 15:58:42 +0530

    Time: 10 ms (5.47%), Space: 8.2 MB (90.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,47 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+         // O(N^2)
+        // int min_index;
+        // for(int i=0;i<nums.size();i++){
+        //     min_index=i;
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[j]<=nums[min_index]){
+        //             min_index=j;
+        //         }
+        //     }
+        //     int temp=nums[i];
+        //     nums[i]=nums[min_index];
+        //     nums[min_index]=temp;
+        
+//         O(N) 2nd Method
+//         int zero=0,one=0,two=zero;
+//         for(int i=0;i<nums.size();i++){
+//             switch(nums[i]){
+//                 case 0:
+//                     zero++;
+//                     break;
+//                 case 1:
+//                     one++;
+//                     break;
+//                 case 2:
+//                     two++;
+//                     break;
+//             }
+//         }
+//         for(int i =0;i<nums.size();i++){
+//             if(zero!=0){
+//                 nums[i]=0;
+//                 zero--;
+//             }
+//             else if(one!=0){
+//                 nums[i]=1;
+//                 one--;
+//             }
+//             else nums[i]=2;
+//         }
+        
+//         O(N) Best Method
         int l=0,h=nums.size()-1,mid=0;
         while(mid<=h){
             if(nums[mid]==0) swap(nums[l++],nums[mid++]);
