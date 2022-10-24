313f342f6c94127bb5134ee22d24440b5fa70604
Prashant Kumar Singh <prashant22297@gmail.com>
2022-10-24 07:27:43 +0530

    Time: 594 ms (12.06%), Space: 123.7 MB (99.26%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> positive,negative,ans;
-        for(int i: nums){
-            if(i>=0) positive.push_back(i);
-            else negative.push_back(i);
-        }
-        // ans.push_back(positive[0]);
-        for(int i=0;i<nums.size()/2;i++){
-            ans.push_back(positive[i]);
-            ans.push_back(negative[i]);
+        // Two Pass O(n+n) and space O(N+N)
+        // vector<int> positive,negative,ans;
+        // for(int i: nums){
+        //     if(i>=0) positive.push_back(i);
+        //     else negative.push_back(i);
+        // }
+        // for(int i=0;i<nums.size()/2;i++){
+        //     ans.push_back(positive[i]);
+        //     ans.push_back(negative[i]);
+        // }
+        // return ans;
+        
+//         2nd method
+        int n=nums.size();
+        vector<int> ans(n,0);
+        int indexpos = 0, indexneg=1;
+        for(auto num: nums){
+            if(num>0){
+                ans[indexpos] = num;
+                indexpos+=2;
+            }
+            if(num<0){
+                ans[indexneg] = num;
+                indexneg += 2;
+            }
         }
         return ans;
     }
