64896a7b9de726ab7e63fd080c607e3ababe02a9
Prashant Kumar Singh <prashant22297@gmail.com>
2022-10-25 13:31:16 +0530

    Time: 19 ms (18.16%), Space: 14 MB (73.80%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -25,20 +25,5 @@ public:
             else l=m+1;
         }
         return false;
-        // int l=0,h=nums.size()-1,m;
-        // while(l<=h){
-        //     m=l+(h-l)/2;
-        //     if(nums[m]==target) return true;
-        //     // left is sorted
-        //     if(nums[l]<=nums[m]){
-        //         if(target >= nums[l] && target <= nums[m])h=m-1;
-        //         else l=m+1;
-        //     }
-        //     else if(nums[m]<=nums[h]){
-        //         if(target >= nums[m] && target <= nums[h]) l=m+1;
-        //         else h=m-1;
-        //     }
-        // }
-        // return false;  
     }
 };
