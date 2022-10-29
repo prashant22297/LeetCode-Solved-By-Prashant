a7fe4b802279ab6d21073b8062ef7f5cf133d720
Prashant Kumar Singh <prashant22297@gmail.com>
2022-10-29 20:49:19 +0530

    Time: 3 ms (87.47%), Space: 7 MB (88.33%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long n = s.length();
+        long long i=0;
+        int positive=1;
+        while(i<n && s[i]==' ')i++;
+        if(i<n && s[i]=='-') {
+            cout<<s[i];
+            positive = -1; 
+            i++;
+        }
+        else if(i<n && s[i]=='+')
+            i++;
+        long long ans=0;
+        while(i<n){
+            if(s[i]>='0' && s[i]<='9'){
+            ans=ans*10+(int(s[i])-48);
+                if(ans> 2147483647) {
+                ans=2147483648;
+                break;
+            }
+            }
+            else {
+                return ans*positive;
+            }
+            
+            i++;
+        }
+        if(positive==-1) ans*=(-1);
+        if(ans<-2147483648) ans=-2147483648;
+        else if(ans>2147483647) ans=2147483647;
+        return (int)ans;
+    }
+};
