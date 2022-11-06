24a96d29c78551366cd68c66a9790c0232c3f42e
Prashant Kumar Singh <prashant22297@gmail.com>
2022-11-06 08:57:01 +0530

    Time: 47 ms (14.30%), Space: 15.8 MB (92.93%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -4,13 +4,8 @@ public:
         int n=letters.size(),s=0,e=n-1,m;
         while(s<=e){
             m=s+(e-s)/2;
-            if(letters[m]==target){
-                s=m+1;
-            // return letters[(m+1)%n];
-            }
-            else if(letters[m]<target){
-                s=m+1;
-            }
+            if(letters[m]==target) s=m+1;
+            else if(letters[m]<target) s=m+1;
             else e=m-1;
         }
         return letters[s%n];
