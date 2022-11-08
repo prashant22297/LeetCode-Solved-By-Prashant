ecc44c34a40444fcc2dc7248eba8ee0c52ecff36
Prashant Kumar Singh <prashant22297@gmail.com>
2022-11-08 18:34:45 +0530

    Time: 85 ms (69.30%), Space: 18.8 MB (95.48%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,7 +3,7 @@ public:
    int minEatingSpeed(vector<int>& piles, int H) {
         int l = 1, r = 1000000000;
         while (l <= r) {
-            long long m = (l + r) / 2, total = 0;
+            long long m = (l)+(r-l) / 2, total = 0;
             for (int p : piles)
                 total += (p + m - 1) / m;
             if (total > H)
