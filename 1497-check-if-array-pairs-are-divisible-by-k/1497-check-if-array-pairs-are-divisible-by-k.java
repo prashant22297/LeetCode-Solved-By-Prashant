10f6f6fd12ea76df87d222abab8df04e01dbdceb
Prashant Kumar Singh <prashant22297@gmail.com>
2023-01-28 13:46:48 +0530

    Time: 9 ms (70.18%), Space: 75.8 MB (19.49%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -1,36 +1,14 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
+        int[] freq = new int[k];
         for(int el : arr){
-            int temp = el%k;
-            if(temp < 0) temp += k;
-            if(mp.containsKey(temp)){
-                mp.put((temp),mp.get(temp)+1);
-            }
-            else mp.put(temp,1);
+            int temp=el%k;
+            if(temp<0) temp+=k;
+            freq[temp]++;
         }
-        // System.out.println(mp.toString());
-        for(int key: mp.keySet()){
-            // System.out.println(key+" "+mp.get(key));
-            if(key == 0)
-            {
-                if(mp.get(key)%2!=0){
-                return false;}
-            }
-            else {
-                if(!mp.containsKey(k-key)){
-                    return false;
-                }
-                else {
-                    int p = mp.get(key), q= mp.get(k-key);
-                    if(p!=q) {
-                        // System.out.print(mp.get(key)+" "+mp.get(k-key));
-                        return false;
-                    }
-                }
-            }
-            
-        }
+        if(freq[0]%2!=0) return false;
+        for(int i=1;i<k;i++)
+            if(freq[i]!=freq[k-i]) return false;
         return true;
     }
 }
