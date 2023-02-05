cd024011edd89a3986570f79c376d1f0c99cbaf8
Prashant Kumar Singh <prashant22297@gmail.com>
2023-02-05 19:37:53 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (61.44%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solve(int lc, int n, int lastcopied){
         if(lc == n) return 0;
-        
         if(n%lc ==0){
             lastcopied=lc;
             lc+=lc;
@@ -14,6 +13,19 @@ class Solution {
     }
     public int minSteps(int n) {
         if(n==1) return 0;
-        return solve(1,n,0);
+        int ans=0;
+        for(int i=2;i*i<=n;){
+            if(n%i==0){
+                ans+=i;
+                n/=i;
+            }else{
+                i++;
+            }
+        }
+        if(n!=1){
+            ans+=n;
+        }
+        return ans;
+        // return solve(1,n,0);
     }
 }
