ae4373d07682f751f477ca3b37efdaac50f893f2
Prashant Kumar Singh <prashant22297@gmail.com>
2023-02-06 15:46:41 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (90.43%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.java
+++ 0969-pancake-sorting/0969-pancake-sorting.java
@@ -2,17 +2,16 @@ class Solution {
     public List<Integer> pancakeSort(int[] arr) {
         int n=arr.length;
         List<Integer> ans = new ArrayList<>();
-        int k=n-1, max_el=0,max_idx=0;int t=10;
-        while(k>=0){
+        int k=n-1, max_el=0,max_idx=0;
+        while(k>0){
             max_el=0;
-            System.out.print(k+" ");
+            // System.out.print(k+" ");
             for(int i=0;i<=k;i++){
                 if(max_el<arr[i]){
                     max_el=arr[i];
                     max_idx = i;
                 }
             }
-            System.out.println(max_el+" "+max_idx);
             if(max_idx==0) {
                 ans.add(k+1);
                 reverse(0,k+1,arr);
@@ -22,7 +21,6 @@ class Solution {
                 ans.add(max_idx+1);
                 reverse(0,max_idx+1,arr);
             }
-            
         }
         return ans;
     }
@@ -33,6 +31,5 @@ class Solution {
             arr[i] = arr[j-i-1];
             arr[j-i-1] = temp;            
         }
-        System.out.print(Arrays.toString(arr));
     }
 }
