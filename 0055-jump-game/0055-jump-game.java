0aaaf47ac62f3f1dc1069acaba617ef0ea391227
Prashant Kumar Singh <prashant22297@gmail.com>
2023-02-12 20:50:15 +0530

    Time: 405 ms (12.29%), Space: 43.6 MB (12.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,25 @@
 class Solution {
     
     public boolean canJump(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>ans) return false;
-            ans=Math.max(ans,i+nums[i]);
-        }
-        return true;
-        // static boolean[] dp = new boolean[100000+1];
-        // return solve(nums,0,nums.length-1);
+        // int ans=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(i>ans) return false;
+        //     ans=Math.max(ans,i+nums[i]);
+        // }
+        // return true;
+        if(nums.length==1) return true;
+        Boolean[] dp = new Boolean[nums.length];
+        // Arrays.fill(dp,false);
+        return solve(nums,0,nums.length-1,dp);
     }
-    // public boolean solve(int[] nums, int first, int last){
-    //     if(first>=last) return true;
-    //     if(dp[first] == true) return true;
-    //     // boolean temp=false;
-    //     for(int i=1;i<=nums[first];i++){
-    //         // temp=solve(nums,first+i;last);
-    //         if(solve(nums,first+i,last) == true) return dp[first]=true;
-    //     }
-    //     return dp[first]=false;
-    // }
+    public boolean solve(int[] nums, int first, int last,Boolean[] dp){
+        if(first>=last) return dp[first]=true;
+        if(dp[first] != null) return dp[first];
+        // boolean temp=false;
+        for(int i=1;i<=nums[first];i++){
+            // temp=solve(nums,first+i;last);
+            if(solve(nums,first+i,last,dp) == true) return dp[first]=true;
+        }
+        return dp[first]=false;
+    }
 }
