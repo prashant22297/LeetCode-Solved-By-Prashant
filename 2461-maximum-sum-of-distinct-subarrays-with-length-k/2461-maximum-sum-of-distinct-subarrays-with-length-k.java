e005a573da6b7c28fc7784e0af60798d814c6d63
Prashant Kumar Singh <prashant22297@gmail.com>
2023-02-13 18:54:26 +0530

    Time: 5 ms (99.78%), Space: 58.7 MB (62.31%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,18 +1,18 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int[] freq = new int[100000+1];
-        Arrays.fill(freq,0);
+        boolean[] freq = new boolean[100000+1];
+        // Arrays.fill(freq,0);
         int l=0,r=0,i=0;
         long sum=0,ans=0;
         while(r<nums.length){
             sum+=nums[r];
             // freq[nums[r]]++;
-            while(freq[nums[r]]>0 || r-l+1>k){
-                freq[nums[l]]--;
+            while(freq[nums[r]] || r-l+1>k){
+                freq[nums[l]]=false;
                 sum-=nums[l];
                 l++;
             }
-            freq[nums[r]]++;
+            freq[nums[r]]=true;;
             
             if(r-l+1==k) ans=Math.max(ans,sum);
             r++;
