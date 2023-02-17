2268ca6c87824459d201726d4e1144e521180cff
Prashant Kumar Singh <prashant22297@gmail.com>
2023-02-17 11:18:18 +0530

    Time: 807 ms (8.12%), Space: 43.1 MB (57.11%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,16 +1,12 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> viss = new ArrayList<>();
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         boolean[] vis = new boolean[nums.length];
         solve(0,nums.length,ans,new ArrayList<>(),nums,vis);
-        for(List i : ans){
-            viss.add(new ArrayList<>(i));
-        }
-        return viss;
+        return ans;
     }
-    public void solve(int idx, int n, Set<List<Integer>> ans, List<Integer> pro,int[] nums, boolean[] vis){
-        if(idx == n){
+    public void solve(int idx, int n, List<List<Integer>> ans, List<Integer> pro,int[] nums, boolean[] vis){
+        if(idx == n && !ans.contains(pro)){
             ans.add(new ArrayList<>(pro));
         }
         for(int i=0;i<n;i++){
