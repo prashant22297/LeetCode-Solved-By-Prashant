83faff3776f2d9facdf63e2b84ee475629fde4a4
Prashant Kumar Singh <prashant22297@gmail.com>
2023-02-23 11:55:03 +0530

    Time: 2 ms (40.29%), Space: 55.7 MB (6.10%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,32 +9,54 @@
  * }
  */
 class Solution {
+    ListNode temp;
+    boolean nulll=false;
     public void reorderList(ListNode head) {
-        ListNode p1=head;
-        ListNode p2=head;
-        while(p2.next!=null && p2.next.next!=null){
-            p1=p1.next;
-            p2=p2.next.next;
-        }
-        // System.out.println(p1.val);
-        ListNode current = p1.next, prev=null;
-        p1.next=null;
-        while(current!=null){
-            ListNode next = current.next;
-            current.next=prev;
-            prev=current;
-            current=next;
+        temp=head;
+        solve(head);
+//         ListNode p1=head;
+//         ListNode p2=head;
+//         while(p2.next!=null && p2.next.next!=null){
+//             p1=p1.next;
+//             p2=p2.next.next;
+//         }
+//         // System.out.println(p1.val);
+//         ListNode current = p1.next, prev=null;
+//         p1.next=null;
+//         while(current!=null){
+//             ListNode next = current.next;
+//             current.next=prev;
+//             prev=current;
+//             current=next;
+//         }
+        
+//         ListNode cur1 = head;
+//         ListNode cur2 = prev;
+//         while(cur1!=null && cur2!=null){
+//             ListNode next1 = cur1.next;
+//             ListNode next2 = cur2.next;
+//             cur1.next=cur2;
+//             cur2.next=next1;
+//             cur1=next1;
+//             cur2=next2;
+        // }
+    }
+    public void solve(ListNode head){
+        if(head==null) return;
+        solve(head.next);
+        
+        if(nulll==false){
+        ListNode next = temp.next;
+        temp.next=head;
+        head.next=next;
+        temp=next;
         }
         
-        ListNode cur1 = head;
-        ListNode cur2 = prev;
-        while(cur1!=null && cur2!=null){
-            ListNode next1 = cur1.next;
-            ListNode next2 = cur2.next;
-            cur1.next=cur2;
-            cur2.next=next1;
-            cur1=next1;
-            cur2=next2;
+        if(temp!=null && temp.next==head){
+            temp.next=null;
+            nulll=true;
+            return;
         }
+        
     }
 }
