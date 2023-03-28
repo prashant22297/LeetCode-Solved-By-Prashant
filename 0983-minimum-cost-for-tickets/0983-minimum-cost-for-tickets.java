41fe59aa329e2683045e47a9c46db3dc48c9e721
Prashant Kumar Singh <prashant22297@gmail.com>
2023-03-28 11:39:09 +0530

    Time: 1 ms (77.14%), Space: 40.1 MB (87.23%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,15 +1,14 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        int[] dp = new int[366];
         int mx = days[days.length-1];
-        Set<Integer> yes = new HashSet<>();
-        for (int day : days) {
-            yes.add(day);
-        }
-        for (int i = 1; i <= mx; i++) {
-            if (yes.contains(i)) 
+        int[] dp = new int[mx+1];
+        int j=0;
+        for (int i=1; i<=mx; i++) {
+            if (days[j]==i){ 
                 dp[i] = Math.min(dp[i-1] + costs[0], Math.min(dp[Math.max(0, i-7)] + costs[1], dp[Math.max(0, i-30)] + costs[2]));
-            else dp[i] = dp[i-1];
+                j++;
+            }
+            else dp[i]=dp[i-1];
         }
         return dp[mx];
     }
