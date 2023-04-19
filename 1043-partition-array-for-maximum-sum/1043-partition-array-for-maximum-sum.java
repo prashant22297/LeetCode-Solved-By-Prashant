1b9167fc96525b49f1efb3e647f7adc83386f059
Prashant Kumar Singh <prashant22297@gmail.com>
2023-04-19 16:39:30 +0530

    Time: 7 ms (48.33%), Space: 42.2 MB (40.99%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -7,11 +7,10 @@ class Solution {
     public int solve(int[] arr, int k ,int i, int n,int[] dp){
         if(i==n) return 0;
         if(dp[i]!=0) return dp[i];
-        int max_value=0, max_sum=0,len=0;
+        int max_value=0, max_sum=0;
         for(int j=i;j<Math.min(n,i+k);j++){
-            len++;
             max_value=Math.max(max_value,arr[j]);
-            int temp_sum=(len*max_value)+solve(arr,k,j+1,n,dp);
+            int temp_sum=((j-i+1)*max_value)+solve(arr,k,j+1,n,dp);
             max_sum=Math.max(max_sum,temp_sum);
         }
         return dp[i]=max_sum;
