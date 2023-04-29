08a7e940a1cfda316052f7a1fa9f91ee9466fd71
Prashant Kumar Singh <prashant22297@gmail.com>
2023-04-29 14:51:27 +0530

    Time: 2 ms (90.86%), Space: 42.8 MB (20.36%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,21 +1,29 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> st = new Stack();
-        for(char ch : s.toCharArray()){
-            // System.out.println(ch);
-            if(st.isEmpty()) st.push(ch);
-            else if((Character.isUpperCase(st.peek()) && Character.isLowerCase(ch)) && (Character.toLowerCase(st.peek())==ch)){
-                st.pop();
+        // Method 1:
+        for(int i=0;i<s.length()-1;i++){
+            if(Math.abs(s.charAt(i)-s.charAt(i+1))==32){
+                return makeGood(s.substring(0,i)+s.substring(i+2));
             }
-            else if((Character.isLowerCase(st.peek()) && Character.isUpperCase(ch)) && (Character.toUpperCase(st.peek())==ch)){
-                st.pop();
-            }
-            else st.push(ch);
         }
-        StringBuilder ans = new StringBuilder();
-        while(!st.isEmpty()){
-            ans.append(st.pop());
-        }
-        return ans.reverse().toString();
+        return s;
+        // Method 2:
+        // Stack<Character> st = new Stack();
+        // for(char ch : s.toCharArray()){
+        //     // System.out.println(ch);
+        //     if(st.isEmpty()) st.push(ch);
+        //     else if((Character.isUpperCase(st.peek()) && Character.isLowerCase(ch)) && (Character.toLowerCase(st.peek())==ch)){
+        //         st.pop();
+        //     }
+        //     else if((Character.isLowerCase(st.peek()) && Character.isUpperCase(ch)) && (Character.toUpperCase(st.peek())==ch)){
+        //         st.pop();
+        //     }
+        //     else st.push(ch);
+        // }
+        // StringBuilder ans = new StringBuilder();
+        // while(!st.isEmpty()){
+        //     ans.append(st.pop());
+        // }
+        // return ans.reverse().toString();
     }
 }
