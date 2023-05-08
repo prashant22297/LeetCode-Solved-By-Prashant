9e1a7f7776ecd465fca344f9a60074da70b44675
Prashant Kumar Singh <prashant22297@gmail.com>
2023-05-08 11:00:09 +0530

    Time: 14 ms (15.89%), Space: 56.2 MB (13.29%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,26 +1,29 @@
 class Solution {
-    List<List<String>> ans = new ArrayList<>();
     public List<List<String>> partition(String s) {
-        solve(0,s,new ArrayList<>());
+        List<List<String>> ans = new ArrayList<>();
+        solve(s,0,ans, new ArrayList<>());
         return ans;
     }
-    void solve(int idx, String s, List<String> unpro){
-        if(idx==s.length()){
-            ans.add(new ArrayList(unpro));
-            return;
-        }
-        for(int i=idx;i<s.length();i++){
-            if(isPali(s,idx,i)){
-                unpro.add(s.substring(idx,i+1));
-                solve(i+1,s,unpro);
-                unpro.remove(unpro.size()-1);
-            }
-        }
-    }
-    boolean isPali(String s, int start, int end){
-        while(start<=end){
-            if(s.charAt(start++)!=s.charAt(end--)) return false;
+    public boolean isPali(String s){
+        String ss= s;
+        int j=0;
+        for(int i=ss.length()-1;i>=0;i--){
+            if(ss.charAt(i)!=s.charAt(j++)) return false;
         }
         return true;
+    }
+    public void solve(String s, int idx, List<List<String>> ans,List<String> curr){
+        if(idx >= s.length()){
+            ans.add(new ArrayList(curr));
+            return;
+        }
+        
+        for(int i =idx; i< s.length();i++){
+            if(isPali(s.substring(idx,i+1))){
+                curr.add(s.substring(idx,i+1));
+                solve(s,i+1,ans,curr);
+                curr.remove(curr.size()-1);
+            }
+        }
     }
 }
