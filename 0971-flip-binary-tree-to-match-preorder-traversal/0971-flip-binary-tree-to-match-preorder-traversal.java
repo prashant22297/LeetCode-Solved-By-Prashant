e0ae82325c4f24084a1b64d77443106d33050000
Prashant Kumar Singh <prashant22297@gmail.com>
2023-06-02 11:32:20 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (83.33%) - LeetHub

--- 0971-flip-binary-tree-to-match-preorder-traversal/0971-flip-binary-tree-to-match-preorder-traversal.java
+++ 0971-flip-binary-tree-to-match-preorder-traversal/0971-flip-binary-tree-to-match-preorder-traversal.java
@@ -19,35 +19,20 @@ class Solution {
     public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
         ans = new ArrayList<>();
         solve(root,voyage);
-        if(ans.contains(-1)){
-            ans.clear();
-            ans.add(-1);
-            return ans;
-        }
         return ans;
     }
-//     private void swap(TreeNode root){
-//         TreeNode temp=root.left;
-//         root.left=root.right;
-//         root.right=temp;
-        
-//     }
     private void solve(TreeNode root, int[] voyage){
-        if(root==null) return;
-        // ;
-        System.out.println(root.val+" "+voyage[idx]);
+        if(root==null || (ans.size()!=0 && ans.get(0)==-1)) return;
         if(root.val!=voyage[idx++]){
             ans = new ArrayList<Integer>(Arrays.asList(-1));
             return;
         } 
         else if(root.val==voyage[idx-1] && root.left != null && root.left.val!=voyage[idx]){
             ans.add(root.val);
-            // idx++;
             solve(root.right,voyage);
             solve(root.left,voyage);
         }
         else {
-            // idx++;
             solve(root.left,voyage);
             solve(root.right,voyage);
         }
