df6c9b2c04ba863853f7d64511f5cc0ed3aec124
Prashant Kumar Singh <prashant22297@gmail.com>
2023-06-09 11:20:13 +0530

    Time: 2 ms (96.51%), Space: 43.7 MB (12.72%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,22 +15,19 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return solve(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<inorder.length;i++)
+            hm.put(inorder[i],i);
+        return solve(inorder,0,inorder.length-1,postorder,0,postorder.length-1,hm);
     }
     
-    private TreeNode solve(int[] in,int is, int ie, int[] post,int ps, int pe){
+    private TreeNode solve(int[] in,int is, int ie, int[] post,int ps, int pe,HashMap<Integer,Integer> hm){
         if(ps>pe || is>ie) return null;
         TreeNode root = new TreeNode(post[pe]);
-        int i_pos=0;
-        for(int i=0;i<in.length;i++){
-            if(in[i]==post[pe]){
-                i_pos=i;
-                break;
-            }
-        }
+        int i_pos=hm.get(post[pe]);
         int t_ele  = i_pos-is;
-        root.left = solve(in,is,i_pos-1,post,ps,ps+t_ele-1);
-        root.right = solve(in,i_pos+1,ie,post,ps+t_ele,pe-1);
+        root.left = solve(in,is,i_pos-1,post,ps,ps+t_ele-1,hm);
+        root.right = solve(in,i_pos+1,ie,post,ps+t_ele,pe-1,hm);
         return root;
         
         
