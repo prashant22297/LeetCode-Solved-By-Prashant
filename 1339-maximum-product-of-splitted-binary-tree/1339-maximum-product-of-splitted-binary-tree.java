e8d0aa22f730b4331111ee4ecc38f2827139f085
Prashant Kumar Singh <prashant22297@gmail.com>
2023-06-13 11:50:53 +0530

    Time: 10 ms (90.31%), Space: 56.5 MB (82.56%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -24,11 +24,9 @@ class Solution {
         if(root==null) return 0;
         int left = solve(root.left,total_sum);
         int right = solve(root.right,total_sum);
-        long p1 = (Math.abs(total_sum-left)%1000000007)*(left%1000000007);
-        long p2 = (Math.abs(total_sum-right)%1000000007)*(right%1000000007);
-        // long p = Math.abs(total_sum-left)>Math.abs(total_sum-right)?Math.abs(total_sum-left)*left:Math.abs(total_sum-right)*right;
+        long p1 = (total_sum-left)*left;
+        long p2 = (total_sum-right)*right;
         product = Math.max(product,Math.max(p1,p2));
-        
         return left+right+root.val;
     }
     private long total(TreeNode root){
