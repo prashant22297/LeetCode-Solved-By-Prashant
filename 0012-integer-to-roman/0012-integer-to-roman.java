a9db50385e91454ec44ed128e5110b4e80cce70f
Prashant Kumar Singh <prashant22297@gmail.com>
2023-07-01 11:42:24 +0530

    Time: 5 ms (80.44%), Space: 43.6 MB (44.94%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,7 +3,7 @@ class Solution {
         int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] name = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
-        StringBuffer ans = new StringBuffer();
+        StringBuilder ans = new StringBuilder();
         for(int i=0;i<value.length;i++){
             while(num>=value[i]){
                 ans.append(name[i]);
