c9ef87a9478f906ff7dac9c1b35d3477147c5270
Prashant Kumar Singh <prashant22297@gmail.com>
2023-08-25 23:57:38 +0530

    Time: 1 ms (91.57%), Space: 42.9 MB (15.93%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,29 +1,29 @@
 class Solution { 
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length()+s2.length()!=s3.length()) return false;
-        int[][][] memo = new int[201][201][201];
+        int[][] memo = new int[101][101];
         return solve(s1,s2,s3,0,0,0,memo);
     }
     
-    private boolean solve(String a,String b, String c, int i , int j ,int k, int[][][] memo){
-        if(i==a.length() && j==b.length() && k==c.length() ){
+    private boolean solve(String a,String b, String c, int i , int j ,int k, int[][] memo){
+        if(i==a.length() && j==b.length() && (i+j)==c.length() ){
             return true;
         }
-        if(memo[i][j][k]!=0){
-            return memo[i][j][k]==-1?false:true;
+        if(memo[i][j]!=0){
+            return memo[i][j]==-1?false:true;
         }
         boolean aa = false;
         boolean bb = false;
-        if(i<a.length() && a.charAt(i)==c.charAt(k)){
-            aa=solve(a,b,c,i+1,j,k+1,memo);
+        if(i<a.length() && a.charAt(i)==c.charAt(i+j)){
+            aa=solve(a,b,c,i+1,j,i+j+1,memo);
         }
-        if(j<b.length() && b.charAt(j)==c.charAt(k)){
-            bb=solve(a,b,c,i,j+1,k+1,memo);
+        if(j<b.length() && b.charAt(j)==c.charAt(i+j)){
+            bb=solve(a,b,c,i,j+1,i+j+1,memo);
         }
         if((aa || bb)==true){
-            memo[i][j][k]=1;
+            memo[i][j]=1;
         }
-        else memo[i][j][k]=-1;
+        else memo[i][j]=-1;
         return ((aa || bb));
     }
 }
