5d7241e69b0660016a7add4143ba5c7cf0c408b4
Prashant Kumar Singh <prashant22297@gmail.com>
2023-09-06 22:47:05 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (9.58%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,7 +14,7 @@ class Node {
 */
 
 class Solution {
-    Map<Node,Node> mp = new HashMap<>();
+    // Map<Node,Node> mp = new HashMap<>();
     public Node copyRandomList(Node head) {
 //         if(head == null) return null;
 //         if(mp.containsKey(head)) return mp.get(head);
@@ -25,31 +25,76 @@ class Solution {
 //         temp.random = copyRandomList(head.random);
 //         return temp;
         
-        if(head == null) return null;
         
+        // Method 2 TC--O(N) SC--O(N) for map
+//         if(head == null) return null;
+        
+//         Node curr = head;
+//         Node newCurr = null;
+//         Node newHead = null;
+//         while(curr!=null){
+//             Node temp = new Node(curr.val);
+//             mp.put(curr,temp);
+//             if(newHead==null) {
+//                 newHead = temp;
+//                 newCurr = temp;
+//             }
+//             else{
+//                 newCurr.next = temp;
+//                 newCurr=newCurr.next; ;
+//             }
+//             curr=curr.next;
+//         }
+//         curr = head;
+//         newCurr = newHead;
+//         while(curr!=null){
+//             newCurr.random = mp.get(curr.random);
+//             curr=curr.next;
+//             newCurr=newCurr.next;
+//         }
+//         return newHead;
+        
+        // Method 3  TC-O(N),,, SC -- O(N) OR O(1) Topic for debate
+        
+        if(head == null) return null;
         Node curr = head;
-        Node newCurr = null;
-        Node newHead = null;
         while(curr!=null){
-            Node temp = new Node(curr.val);
-            mp.put(curr,temp);
-            if(newHead==null) {
-                newHead = temp;
-                newCurr = temp;
+            Node currNext = curr.next;
+            Node newNode = new Node(curr.val);
+            curr.next=newNode;
+            newNode.next=currNext;
+            curr=curr.next.next;
+            
+        }
+        // curr = head;
+        // while(curr!=null){
+        //     System.out.print(curr.val+" ");
+        //     curr=curr.next;
+        // }
+        
+        
+        curr=head;
+        while(curr!=null && curr.next!=null){
+            if(curr.random==null){
+                curr.next.random = null;
             }
             else{
-                newCurr.next = temp;
-                newCurr=newCurr.next; ;
+                curr.next.random = curr.random.next;
             }
-            curr=curr.next;
+            curr=curr.next.next;
         }
+        
         curr = head;
-        newCurr = newHead;
-        while(curr!=null){
-            newCurr.random = mp.get(curr.random);
+        Node newHead = head.next;
+        Node newCurr = newHead;
+        
+        while(curr!=null && newCurr!=null){
+            curr.next=curr.next==null?null:curr.next.next;
+            newCurr.next=newCurr.next==null?null:newCurr.next.next;
             curr=curr.next;
-            newCurr=newCurr.next;
+            newCurr = newCurr.next;
         }
+        
         return newHead;
     }
 }
