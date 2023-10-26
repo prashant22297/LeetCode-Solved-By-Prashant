23bcc7353e9174306a0b60cb5cd362b91bf266c0
Prashant Kumar Singh <prashant22297@gmail.com>
2023-10-26 13:36:33 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (14.87%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -23,18 +23,39 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+        // Method 1 TC O(N) + SC O(N)
+        // if(root==null) return root;
+        // Queue<Node> q = new LinkedList<>();
+        // q.add(root);
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     for(int i=0;i<size;i++){
+        //         Node temp = q.remove();
+        //         if(i<size-1) temp.next=q.peek();
+        //         if(temp.left!=null) q.add(temp.left);
+        //         if(temp.right!=null) q.add(temp.right);
+        //     }
+        // }
+        // return root;
+        
+        
+        
+        
+        // Method 2 TC O(N) + SC O(1)
         if(root==null) return root;
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                Node temp = q.remove();
-                if(i<size-1) temp.next=q.peek();
-                if(temp.left!=null) q.add(temp.left);
-                if(temp.right!=null) q.add(temp.right);
+        Node leftMost = root;
+        while(leftMost.left!=null){
+            Node curr = leftMost;
+            while(curr!=null){
+                curr.left.next=curr.right;
+                if(curr.next!=null){
+                    curr.right.next=curr.next.left;
+                }
+                curr=curr.next;
             }
+            leftMost=leftMost.left;
         }
         return root;
+        
     }
 }
