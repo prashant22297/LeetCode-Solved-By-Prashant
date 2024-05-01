32b5ddd82a399c5cc1599d9fd390d4108cec0e09
Prashant Kumar Singh <prashant22297@gmail.com>
2024-05-01 19:30:26 +0530

    Time: 45 ms (63.75%), Space: 67.3 MB (47.89%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -1,68 +1,44 @@
-
-//     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-//         Map<Integer, List<Integer>> adj = new HashMap<>();
-//         for (int i = 0; i < n; i++) {
-//             adj.put(i, new ArrayList<>());
-//         }
-//         for (int[] edge : edges) {
-//             int u = edge[0];
-//             int v = edge[1];
-//             adj.get(u).add(v);
-//             adj.get(v).add(u);
-//         }
-//         int[][] dp = new int[n][n];
-//         for (int[] i : dp) Arrays.fill(i, -1);
-//         // solve();
-
-//         int[] ans = new int[n];
-//         return ans;
-//     }
-
-
 class Solution {
     int N;
     int[] ans, count;
-    List<List<Integer>> gr;
+    List<List<Integer>> adj;
 
-    void dfs(int node, int par) {
-        for (int child : gr.get(node)) {
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        N = n;
+        ans = new int[n];
+        count = new int[n];
+        Arrays.fill(count, 1);
+        adj = new ArrayList(n);
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList());
+        }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        dfs1(0, -1);
+        dfs2(0, -1);
+        return ans;
+    }
+
+    public void dfs1(int node, int par) {
+        for (int child : adj.get(node)) {
             if (child != par) {
-                dfs(child, node);
-                count[node] += count[child];
-                ans[node] += ans[child] + count[child];
+                dfs1(child, node);
+                count[node] = count[node] + count[child];
+                ans[node] = ans[node] + count[child] + ans[child];
             }
         }
     }
 
-    void dfs2(int node, int par) {
-        for (int child : gr.get(node)) {
+    public void dfs2(int node, int par) {
+        for (int child : adj.get(node)) {
             if (child != par) {
                 ans[child] = ans[node] - count[child] + (N - count[child]);
                 dfs2(child, node);
             }
         }
-    }
-
-    public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        N = n;
-        gr = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            gr.add(new ArrayList<>());
-        }
-
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            gr.get(u).add(v);
-            gr.get(v).add(u);
-        }
-
-        ans = new int[n];
-        count = new int[n];
-        Arrays.fill(count, 1);
-
-        dfs(0, -1);
-        dfs2(0, -1);
-
-        return ans;
     }
 }
