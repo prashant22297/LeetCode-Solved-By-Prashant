a6a730d945950e055895fe0f13b5adfea150a7c7
Prashant Kumar Singh <prashant22297@gmail.com>
2024-05-28 21:07:49 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (67.90%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,11 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        String ans="";
-        for(char ch : s.toCharArray()){
-            if(Character.isUpperCase(ch))
-                ans+=Character.toLowerCase(ch);
-            else ans+=ch;
-        }
-        return ans;
+        // Method 1:
+        // String ans="";
+        // for(char ch : s.toCharArray()){
+        //     if(Character.isUpperCase(ch))
+        //         ans+=Character.toLowerCase(ch);
+        //     else ans+=ch;
+        // }
+        // return ans;
+        
+        // Method 2:
+        return s.toLowerCase();
     }
 }
