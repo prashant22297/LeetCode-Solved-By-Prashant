24462ea96ad23028bef6156ca14770867a125a5e
Prashant Kumar Singh <prashant22297@gmail.com>
2024-06-16 22:14:35 +0530

    Time: 1 ms (97.42%), Space: 45.1 MB (65.13%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,49 +1,30 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int n = nums.length;
-        if(n==1) return 0;
-        else {
-            int s=0,e=nums.length-1;
-            int leftSmall= Integer.MAX_VALUE,rightGreater=Integer.MIN_VALUE;
-            int small= Integer.MAX_VALUE,big=Integer.MIN_VALUE;
-            while(s<n-1 && nums[s]<=nums[s+1]){
-                if(nums[s]<leftSmall)
-                    leftSmall=nums[s];
-                s++;
-               
-            }
-            while(e>0 && nums[e]>=nums[e-1]){
-                if(nums[s]>rightGreater)
-                    rightGreater=nums[e];
-                e--;
-            }
-            // System.out.println(leftSmall+" "+rightGreater);
-            // System.out.println(s+" "+e);
-            if(s>e) return 0;
-            else {
-                for(int i=s;i<=e;i++){
-                    if(nums[i]<small){
-                        small =nums[i];
-                    }
-                    if(nums[i]>big){
-                        big=nums[i];
-                    }
-                }
-                int id1=0,id2=0;
-                for(int i = 0; i < n; i++){
-                    if(nums[i] > small){   
-                        id1 = i;
-                        break;
-                    }
-                }
-                for(int i = 0; i < n; i++){
-                    if(nums[i] < big){
-                        id2 = i;
-                    }
-                }
-                // System.out.println(small+" "+big);
-                return id2-id1+1;
-            }
+        int l =0;
+        int r =n-1;
+        while(l<n-1 && nums[l]<=nums[l+1]){
+            l++;
         }
+        if(l==r) return 0;
+        while(r>0 && nums[r]>=nums[r-1]){
+            r--;
+        }
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=l;i<=r;i++){
+            min= Math.min(min,nums[i]);
+            max =Math.max(max,nums[i]);
+        }
+        
+        while(l > 0 && min<nums[l-1]){
+            l--;
+            
+        }
+        while(r < n - 1 && max > nums[r+1]){
+            r++;
+        }
+
+        return r - l + 1;
     }
 }
