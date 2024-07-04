d79b3c2d004281e5e11821516d8fb40b1ff8703f
Prashant Kumar Singh <prashant22297@gmail.com>
2024-07-04 17:22:00 +0530

    Time: 2 ms (79.36%), Space: 41.6 MB (41.60%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,42 +1,89 @@
 class Solution {
-    public String reorganizeString(String s) {
-        int[] freq = new int[26];
-        for(Character c : s.toCharArray()){
-            freq[c-97]++;
+    
+    // Method 2:
+    public class pair{
+        char ch;
+        int freq;
+        pair(char ch, int freq){
+            this.ch = ch;
+            this.freq=freq;
         }
-        int max=0;
-        char letter='0';
-        for(Character c : s.toCharArray()){
-            if(freq[c-97]>max){
-                max = freq[c-97];
-                letter = c;
-            }
-        }
-        if(max >(s.length()+1)/2)
-            return "";
-        char[] ch = new char[s.length()];
-        int idx=0;
-        for(;idx<s.length();idx+=2){
-            if(max>0){
-                ch[idx]=letter;
-                max--;
-                freq[letter-97]--;
-            }
-            else break;
-        }
-        for(int i=0;i<26;i++){
-            while(freq[i]>0){
-                if(idx>=s.length()){
-                    idx=1;
-                }
-                ch[idx]=(char)(i+97);
-                idx+=2;
-                freq[i]--;
-            }
-        }
-        // for(char c: ch){
-        //     System.out.println(c);
-        // }
-        return String.valueOf(ch);
     }
+    public class compare implements Comparator<pair>{
+        public int compare(pair a , pair b){
+            if(a.freq<b.freq){
+                return 1;
+            }
+            return -1;
+        }
+    }
+    public String reorganizeString(String s) {
+        PriorityQueue<pair> pq = new PriorityQueue<>(new compare());
+        
+        int[] freq = new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                pair temp = new pair((char)(i+'a'),freq[i]);
+                pq.add(temp);
+            }
+        }
+        pair temp = pq.peek();
+        if(temp.freq>((s.length()+1)/2)) return "";
+        int i=0;
+        char[] ans = new char[s.length()];
+        while(!pq.isEmpty()){
+            temp = pq.remove();
+            int f = temp.freq;
+            char ch = temp.ch;
+            while(f-->0){
+                ans[i]=ch;
+                i+=2;
+                if(i>=s.length()){
+                    i=1;
+                }
+            }
+        }
+        return String.valueOf(ans);
+    }
+    
+    
+    
+    // Method 1:
+//      public String reorganizeString(String s) {
+//         int[] freq = new int[26];
+//         int max = 0;
+//         char letter='_';
+//         for(int i=0;i<s.length();i++){
+//             freq[s.charAt(i)-'a']++;
+//             if(freq[s.charAt(i)-'a']>max){
+//                 max = freq[s.charAt(i)-'a'];
+//                 letter = s.charAt(i);
+//             }
+//         }
+//         if(max > ((s.length()+1)/2)) return "";
+//         int idx=0;
+//         char[] ans = new char[s.length()];
+//         while(max-->0){
+//             ans[idx]=letter;
+//             idx+=2;
+//             freq[letter-97]--;
+//         }
+//         for(int i = 0;i<26;i++){
+//             while(freq[i]>0){
+//                 if(idx>=s.length()){
+//                     idx=1;
+//                 }
+//                 ans[idx]=(char)(i+97);
+//                 freq[i]--;
+//                 idx+=2;
+                
+//             }
+//         }
+//         return String.valueOf(ans);
+//     }
 }
