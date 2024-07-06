2fa02b553e63ee7561f20408785ba4137158fd4b
Prashant Kumar Singh <prashant22297@gmail.com>
2024-07-06 19:10:22 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (46.33%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.java
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -1,8 +1,22 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int onepasstime=n-1;
-        int totalpass = time/onepasstime;
-        time=time%(n-1);
-        return totalpass%2==0?time+1:n-time;
+        int i = 1;
+        while(time != 0){
+            for( ; i < n ; i++ ) {
+                if(time != 0){
+                    time--;
+                }
+                else
+                    break;
+            }
+            for( ; i > 1 ; i-- ) {
+                if(time != 0) {
+                    time--;
+                }
+                else
+                    break;
+            }
+        }
+        return i;
     }
 }
