b9891727a599aa14534db1775257e1bf09706802
Prashant Kumar Singh <prashant22297@gmail.com>
2024-09-19 19:27:53 +0530

    Time: 2 ms (68.85%), Space: 42.8 MB (37.39%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,25 +1,21 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String exp) {
+    public List<Integer> diffWaysToCompute(String expression) {
         List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<exp.length();i++){
-            if(exp.charAt(i)=='-' || exp.charAt(i)=='+' || exp.charAt(i)=='*'){
-                String p1=exp.substring(0,i);
-                String p2=exp.substring(i+1);
-                List<Integer> part1 = diffWaysToCompute(p1);
-                List<Integer> part2 = diffWaysToCompute(p2);
-                // System.out.print(p1);
-                // System.out.println(p2);
-                for(int a: part1){
-                    for(int b: part2){
+        for(int i=0;i<expression.length();i++){
+            if(expression.charAt(i)=='-' || expression.charAt(i)=='+' || expression.charAt(i)=='*'){
+                List<Integer> p1 = diffWaysToCompute(expression.substring(0,i));
+                List<Integer> p2 = diffWaysToCompute(expression.substring(i+1));
+                for(int j : p1){
+                    for(int k : p2){
                         int temp=0;
-                        if(exp.charAt(i)=='-'){
-                            temp=a-b;
+                        if(expression.charAt(i)=='-'){
+                            temp = j-k;
                         }
-                        else if(exp.charAt(i)=='+'){
-                            temp=a+b;
+                        else if(expression.charAt(i)=='+'){
+                            temp=j+k;
                         }
                         else{
-                            temp=a*b;
+                            temp=j*k;
                         }
                         ans.add(temp);
                     }
@@ -27,7 +23,7 @@ class Solution {
             }
         }
         if(ans.size()==0) {
-            ans.add(Integer.valueOf(exp));
+            ans.add(Integer.valueOf(expression));
         }
         return ans;
     }
