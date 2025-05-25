4e16d92dcb488b07b7f8eb98a4a9391d443049d2
Prashant Kumar Singh <prashant22297@gmail.com>
2025-05-25 07:09:55 +0530

    Time: 3 ms (88.07%) | Memory: 96 MB (22.81%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,24 +1,22 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        int[] diff = new int[n + 1]; // Difference array for efficient range updates
-
-        // Process each query using difference array
-        for (int[] q : queries) {
-            int li = q[0], ri = q[1];
-            diff[li]--;
-            diff[ri + 1]++;
-        }
-
-        int currentDecrement = 0; // Tracks how many times an index is decremented
-
-        // Apply the decrements and check if nums can become zero
-        for (int i = 0; i < n; i++) {
-            currentDecrement += diff[i]; // Apply prefix sum
-            if (nums[i] + currentDecrement > 0) {
-                return false; // If nums[i] can't be reduced to 0, return false
+        int lengthNums = nums.length;
+        int[] diff = new int[lengthNums];
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            diff[l] += 1;
+            if (r + 1 < lengthNums) {
+                diff[r + 1] -= 1;
             }
         }
-        return true; // If all elements are 0, return true
+        int sum = 0;
+        for (int i = 0; i < lengthNums; i++) {
+            sum += diff[i];
+            if (nums[i] > sum) {
+                return false;
+            }
+        }
+        return true;
     }
 }
