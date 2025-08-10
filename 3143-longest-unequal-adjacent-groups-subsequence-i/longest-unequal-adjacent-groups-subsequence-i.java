74b056c7a286494beef4d8f3c97e9e5f8ec3ea1c
Prashant Kumar Singh <prashant22297@gmail.com>
2025-08-10 15:04:12 +0530

    Time: 1 ms (91.83%) | Memory: 45 MB (36.65%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
@@ -1,36 +1,12 @@
 class Solution {
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
         int n = words.length;
-        List<String> startsWithZero = new ArrayList<>();
-        List<String> startsWithOne = new ArrayList<>();
-        boolean zero = true;
-        boolean one = true;
+        List<String> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (zero) {
-                if (groups[i] == 0) {
-                    startsWithZero.add(words[i]);
-                    zero = false;
-                }
-            } else {
-                if (groups[i] == 1) {
-                    startsWithZero.add(words[i]);
-                    zero = true;
-                }
-
-            }
-            if (one) {
-                if (groups[i] == 1) {
-                    startsWithOne.add(words[i]);
-                    one = false;
-                }
-            } else {
-                if (groups[i] == 0) {
-                    startsWithOne.add(words[i]);
-                    one = true;
-                }
-
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.add(words[i]);
             }
         }
-        return startsWithZero.size() > startsWithOne.size() ? startsWithZero : startsWithOne;
+        return ans;
     }
 }
