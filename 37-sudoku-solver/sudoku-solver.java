6b1a914ef0c90e4fcad9240fa55e44e470a50a98
Prashant Kumar Singh <prashant22297@gmail.com>
2025-08-31 21:13:20 +0530

    Time: 157 ms (89.50%) | Memory: 41.4 MB (33.65%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    boolean isValid(char board[][], int row, int col, char digit) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == digit)
+                return false;
+            if (board[i][col] == digit)
+                return false;
+        }
+
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == digit)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    boolean helper(char board[][], int row, int col) {
+        if (row == 9)
+            return true;
+
+        int nr = row;
+        int nc = col + 1;
+
+        if (nc == 9) {
+            nr = row + 1;
+            nc = 0;
+        }
+
+        if (board[row][col] != '.') {
+            return helper(board, nr, nc);
+        }
+
+        for (char digit = '1'; digit <= '9'; digit++) {
+            if (isValid(board, row, col, digit)) {
+                board[row][col] = digit;
+                if (helper(board, nr, nc) == true) {
+                    return true;
+                } else {
+                    board[row][col] = '.';
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
+}
