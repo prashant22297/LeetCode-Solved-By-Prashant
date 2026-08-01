bd75f20b7135b6a891446b3a7a60bd3a2474e81c
Prashant Kumar Singh <prashant22297@gmail.com>
2026-08-02 00:15:21 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,18 +1,23 @@
-public class Solution {
-    public boolean PredictTheWinner(int[] nums) {
-        int left=0,right = nums.length-1,sum = 0;
-        for (int i: nums)
-            sum = sum+i;
-        int p1 = solve(nums, left, right);
-        int p2 = sum-p1;
-        // System.out.println(p1+" "+p2);
-        if (p1 >= p2) return true;
-        return false;
-    }
-    public static int solve(int[] nums, int left, int right) {
-        if (left > right) return 0;
-        int choice1 = nums[left] + Math.min(solve(nums, left + 2, right), solve(nums, left + 1, right - 1));
-        int choice2 = nums[right] + Math.min(solve(nums, left + 1, right - 1), solve(nums, left, right - 2));
-        return Math.max(choice1, choice2);
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                dp[i][j] = Math.max(
+                    nums[i] - dp[i + 1][j],
+                    nums[j] - dp[i][j - 1]
+                );
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
     }
 }
